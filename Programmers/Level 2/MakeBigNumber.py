2483fbe027599e2b01c8dfa49addad74f057ee31
WooYong Kim <wykim970@gmail.com>
2021-01-22 23:58:34 +0900

    21/01/22 solve again

--- Programmers/Level 2/MakeBigNumber.py
+++ Programmers/Level 2/MakeBigNumber.py
@@ -1,40 +1,36 @@
 '''
+어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
 
+예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 
+이 중 가장 큰 숫자는 94 입니다.
+
+문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. 
+number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 
+문자열 형태로 return 하도록 solution 함수를 완성하세요.
 '''
-
-number = 9876
+# Input
+number = 1924
 k = 2
 
-def solution(number,k):
-    answer = ''
-    tmp = [int(n) for n in str(number)]
-    remove = []
-    cnt = 0
-    i = 0
-    idx = 0
+# Stack이 핵심
+# Retry
 
-    while cnt < k:
-        if tmp[i] > tmp[i+1]:
-            remove.append(i+1)
-            cnt += 1
-            i += 1
-            print(remove)
-        elif tmp[i] < tmp[i+1]:
-            remove.append(i)
-            cnt += 1
-            i += 1
-            print(remove)
-        else: 
-            i += 1
-            print(remove)
+def solution(number, k):
+    answer = ''
+    number = [int(i) for i in str(number)]
+    stack = []
+    stack.append(number[0])
+
+    for n in number[1:]:
+        while len(stack) > 0 and stack[-1] < n and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(n)
     
-    print(tmp)
-    for n in range(0,len(remove)):
-        tmp.pop(remove[n]-idx)
-        idx += 1
-    print(tmp)
-    answer = "".join(map(str,tmp))
-  
+    if k != 0:
+        stack = stack[:-k]
+
+    answer = "".join(map(str, stack))
     return answer
 
-print(solution(number,k))
+print(solution(number, k))

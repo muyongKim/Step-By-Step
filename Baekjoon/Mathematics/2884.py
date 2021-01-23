aea56a78f04a9d9f43a963143465fbef1814d2aa
WooYong Kim <wykim970@gmail.com>
2021-01-23 22:54:00 +0900

    21/01/23 solved

---
+++ Baekjoon/Mathematics/2884.py
@@ -0,0 +1,13 @@
+# 알람 시계
+
+h, m = map(int, input().split())
+
+if m - 45 < 0:
+    h -= 1
+    if h<0:
+        h = 23
+    m += 15
+    print("%d %d" %(h,m))
+else: 
+    m -= 45
+    print("%d %d" %(h,m))
--- Programmers/Level 2/Carpet.py
+++ Programmers/Level 2/Carpet.py
@@ -5,14 +5,12 @@ Leo는 카펫을 사러 갔다가 아래 그림과 같이 중앙에는 노란색
 Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 
 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 '''
-# Input
+# # Input
 brown = 24
 yellow = 24
 
 def solution(brown,yellow):
     divisor = []
-    trans = 0
-    vert = 0
 
     for i in range(1, yellow+1):            # yellow의 약수
         if yellow % i == 0:                 # but 실행시간이 길어짐
@@ -22,9 +20,7 @@ def solution(brown,yellow):
 
     for n in range(0, int(len(divisor)/2)+1):
         if brown == 2*divisor[n] + 2*(yellow/divisor[n]+2):
-            trans = divisor[n]+2
-            vert = int(yellow/divisor[n])+2
-            answer = [trans, vert]
+            answer = [divisor[n]+2, int(yellow/divisor[n])+2]
             return answer
 
 print(solution(brown,yellow))

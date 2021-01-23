99f60836a6996b1604dce865c919de349497b313
WooYong Kim <wykim970@gmail.com>
2021-01-23 23:05:43 +0900

    Modify content

--- Programmers/Level 1/CreateStrangeString.py
+++ Programmers/Level 1/CreateStrangeString.py
@@ -1,14 +1,6 @@
-'''
-문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 
-각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 
-각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
-
-제한 사항
-문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
-첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
-
-예) try hello world -> TrY HeLlO WoRlD
-'''
+# 이상한 문자 만들기
+# Input
+s = "try hello world"
 
 def solution(s):
     char_list = ""
@@ -27,4 +19,4 @@ def solution(s):
 
     return char_list
 
-print(solution("try hello world"))
+print(solution(s))
--- Programmers/Level 1/ReverseNumberToArray.py
+++ Programmers/Level 1/ReverseNumberToArray.py
@@ -1,10 +1,6 @@
-'''
-자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 
-예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
-
-제한 조건
-n은 10,000,000,000이하인 자연수입니다.
-'''
+# 자연수 뒤집어 배열로 만들기
+# Input
+n = 12345
 
 def solution (n):
     answer = []
@@ -18,4 +14,4 @@ def solution (n):
             n = int(n/10)
     return answer
 
-print(solution(12345))
+print(solution(n))

640879cd4360fcb494aef29ff42d9d898fd72231
WooYong Kim <wykim970@gmail.com>
2021-01-24 21:41:59 +0900

    21/01/24 solved

---
+++ Baekjoon/Mathematics/1110.py
@@ -0,0 +1,22 @@
+# 더하기 사이클
+# 잦은 형변환 -> %연산으로 대체 가능
+
+N = input()
+first = N
+cycle = 0
+
+if int(N)<10:
+    N = '0'+N
+
+while True:
+    tmp = str(int(N[0])+int(N[1]))
+
+    if int(tmp)>=10:
+        tmp = str(tmp[1])
+    N = N[1]+tmp
+    cycle += 1
+
+    if int(N) == int(first):
+        break
+
+print(cycle)
---
+++ Baekjoon/String/8958.py
@@ -0,0 +1,20 @@
+# OX퀴즈
+
+n = int(input())
+sum_list = []
+
+for i in range(0,n):
+    score = 0
+    sum = 0
+    answer = list(input())
+    
+    for j in answer:
+        if j == 'O':
+            score += 1
+            sum += score
+        else:
+            score = 0
+    sum_list.append(sum)
+
+for j in sum_list:
+    print(j)

c10130aff7bafd4e7459974280be42a7778a4006
WooYong Kim <wykim970@gmail.com>
2021-01-31 18:01:04 +0900

    21/01/31 Programmers Level 1 solved

---
+++ Programmers/Level 1/AdditionMatrix.py
@@ -0,0 +1,16 @@
+# 행렬의 덧셈 2021/01/31
+# Input
+arr1 = [[1],[2]]
+arr2 = [[3],[4]]
+
+def solution(arr1,arr2):
+    answer = []
+    for i in range(len(arr1)):
+        answer.append([])
+        for num1,num2 in zip(arr1[i],arr2[i]):
+            answer[i].append(num1+num2)
+    return answer
+
+print(solution(arr1,arr2))
+
+# 행렬 연산 -> numpy

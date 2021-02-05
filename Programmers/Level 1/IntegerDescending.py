2fe04448a52ff1e81189d636c56ec8c600e64da5
WooYong Kim <wykim970@gmail.com>
2021-02-06 00:09:14 +0900

    Modify content

--- Programmers/Level 1/IntegerDescending.py
+++ Programmers/Level 1/IntegerDescending.py
@@ -1,9 +1,11 @@
 # 정수 내림차순으로 배치하기 2021/02/05
 # Input
-n = list(input())
+n = int(input())
 
 def solution(n):
-    n.sort(reverse=True)
-    return int("".join(n))
+    n = str(n)
+    n_list = [i for i in n]
+    n_list.sort(reverse=True)
+    return int("".join(n_list))
 
 print(solution(n))

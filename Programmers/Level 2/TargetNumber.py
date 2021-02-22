e7afa19a45020bd33f646a0345f929c6f2641002
WooYong Kim <wykim970@gmail.com>
2021-02-22 15:25:27 +0900

    21/02/22 Programmers Level 2 solved

---
+++ Programmers/Level 2/TargetNumber.py
@@ -0,0 +1,22 @@
+# 타겟 넘버 2021/02/21
+# Input
+numbers = [1,1,1,1,1]
+target = 3
+
+def solution(numbers, target):
+    answer = 0
+    stack = [[0,0]]
+
+    while stack:
+        index, value = stack.pop()
+
+        if index < len(numbers):
+            stack.append([index+1, value - numbers[index]])
+            stack.append([index+1, value + numbers[index]])
+        else:
+            if value == target:
+                answer += 1
+    
+    return answer
+
+print(solution(numbers, target))

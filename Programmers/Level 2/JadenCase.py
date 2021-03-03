0baa5bc723e56b145be8f6b05406fd981186adef
WooYong Kim <wykim970@gmail.com>
2021-03-04 03:15:31 +0900

    21/03/04 Programmers Level 2 solved and modify content

--- Programmers/Level 2/JadenCase.py
+++ Programmers/Level 2/JadenCase.py
@@ -1,11 +1,10 @@
 # JadenCase 문자열 만들기 2021/03/03
-# Retry
 # Input
-s = "for the last  week"
+s = "for the last week"
 
 def solution(s):
     answer = []
-    tmp = s.split()
+    tmp = s.split(" ")
 
     for word in tmp:
         answer.append(word.capitalize())

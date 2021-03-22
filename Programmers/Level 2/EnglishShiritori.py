380b9f035f5f195637ebfd86ab9138929f22ad4f
WooYong Kim <wykim970@gmail.com>
2021-03-22 23:27:39 +0900

    21/03/22 영어 끝말잇기 solving

---
+++ Programmers/Level 2/EnglishShiritori.py
@@ -0,0 +1,14 @@
+# 영어 끝말잇기 2021/03/22
+# Input
+n = 2
+words = ["hello", "one", "even", "never", "now", "world", "draw"]
+
+# def solution(n, words):
+#     passed = []
+#     for w, passed_w in zip(words, passed):
+#         if w[0] == passed_w[0] or 
+passed = [1]
+if not passed:
+    print("Empty")
+else:
+    print("Not empty")

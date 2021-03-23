e33828877338252301fe919361cfd1a95167660e
WooYong Kim <wykim970@gmail.com>
2021-03-23 18:11:19 +0900

    21/03/23 영어 끝말잇기 solved

--- Programmers/Level 2/EnglishShiritori.py
+++ Programmers/Level 2/EnglishShiritori.py
@@ -1,14 +1,37 @@
-# 영어 끝말잇기 2021/03/22
+# 영어 끝말잇기 2021/03/23
 # Input
-n = 2
-words = ["hello", "one", "even", "never", "now", "world", "draw"]
+n = 3
+words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
 
-# def solution(n, words):
-#     passed = []
-#     for w, passed_w in zip(words, passed):
-#         if w[0] == passed_w[0] or 
-passed = [1]
-if not passed:
-    print("Empty")
-else:
-    print("Not empty")
+def check_loser(n, cnt):
+    answer = []
+    loser = cnt % n
+
+    if loser == 0:
+        loser = n
+        loop = cnt // n
+    else:
+        loop = cnt // n + 1
+
+    answer.append(loser)
+    answer.append(loop)
+
+    return answer
+
+def solution(n, words):
+    passed = []
+    answer = [0, 0]
+    cnt = 0
+
+    for w in words:
+        cnt += 1
+        if not passed or passed[-1][-1] == w[0]:
+            if w in passed:
+                return check_loser(n, cnt)
+            passed.append(w)
+        elif w[0] != passed[-1][-1]:
+            return check_loser(n, cnt)
+
+    return answer
+
+print(solution(n, words))

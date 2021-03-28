70b5f3855056a921a10a7e212eb2da75b82ee864
WooYong Kim <wykim970@gmail.com>
2021-03-29 00:23:32 +0900

    21/03/28 체육복 solved

--- Programmers/Level 1/GymClothes.py
+++ Programmers/Level 1/GymClothes.py
@@ -1,30 +1,30 @@
 # 체육복 2021/03/28
 # Input
-n = 5
-lost = [2,4]
-reserve = [3]
+n = 7
+lost = [2,4,5,6,7]
+reserve = [1,3,4,5,6,7]
 
 def solution(n, lost, reserve):
     answer = 0
     available = [1 for i in range(n)]
+    tmp = [v for v in reserve]
 
     for i in lost:
         available[i-1] = 0
     
     for i in reserve:
         if available[i-1] == 0:
-            reserve.remove(i)
+            tmp.remove(i)
             available[i-1] += 1
     
-    print(available)
-    print(reserve)
+    reserve = tmp
     
     for i in range(len(available)):
         # 첫번째 값 처리
-        if i == 0 and available[i] == 1 and available[i+1] == 0 and reserve.count(i+1) == 1:
+        if i == 0 and available[i+1] == 0 and reserve.count(i+1) == 1:
             available[i+1] += 1
             reserve.remove(i+1)
-        elif available[i] == 1 and reserve.count(i+1) == 1 and (available[i-1:i] == 0 or available[i+1:i+2] == 0):
+        elif reserve.count(i+1) == 1 and (available[i-1:i] == [0] or available[i+1:i+2] == [0]):
             if available[i-1] == 0:
                 available[i-1] += 1
                 reserve.remove(i+1)

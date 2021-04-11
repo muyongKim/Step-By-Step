5a13e218d962041a763ad82c9040b7c03cac10bf
WooYong Kim <wykim970@gmail.com>
2021-04-11 15:49:13 +0900

    21/04/11 방금그곡 solved

--- Programmers/Level 2/KAKAO/ThatSong.py
+++ Programmers/Level 2/KAKAO/ThatSong.py
@@ -1,21 +1,25 @@
-# 방금그곡 2018 kakao blind recruitment
+# 방금그곡 2018 kakao blind recruitment 2021/04/10
 # Input
 m = "ABC"
-musicinfos = ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+musicinfos = ["13:00,13:10,WORLD,ABC#ABC", "13:00,13:12,WORLD2,ABCDEF"]
 
 def solution(m,musicinfos):
     dic = {}
 
     for info in musicinfos:
         info = info.split(',')
+
+        # 재생시간 
         hour = int(info[1][:2]) - int(info[0][:2])
         minute = int(info[1][3:]) - int(info[0][3:])
         if minute < 0:
             minute += 60
+            hour -= 1
         
         playtime = hour*60 + minute
         info.append(playtime)
         
+        # 라디오에서 재생된 멜로디
         melody = ''
         while playtime:
             for i in range(len(info[3])):
@@ -30,12 +34,14 @@ def solution(m,musicinfos):
                 if playtime == 0:
                     break
         
-        if m in melody:
+        # 멜로디 비교
+        while melody.find(m)+1:
             if melody.find(m) + len(m) < len(melody) and melody[melody.find(m) + len(m)] == '#':
-                melody = melody[len(m)+1:]
+                melody = melody[melody.find(m)+len(m)+1:]
                 continue
             else:
                 dic[info[2]] = info[4]
+                break
 
     if not dic:
         return "(None)"

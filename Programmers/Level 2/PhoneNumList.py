1bfff9634326bbb537a68621b49e17a1a4970e57
WooYong Kim <wykim970@gmail.com>
2021-05-03 00:30:26 +0900

    21/05/03 전화번호 목록 retry

--- Programmers/Level 2/PhoneNumList.py
+++ Programmers/Level 2/PhoneNumList.py
@@ -3,10 +3,11 @@
 phone_book = ["119", "97674223", "1195524421"]
 
 def solution(phone_book):
+    phone_book.sort()
+
     for i in range(len(phone_book)-1):
-        for j in range(i+1,len(phone_book)):
-            if phone_book[i] in phone_book[j]:
-                return False
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
     
     return True
 

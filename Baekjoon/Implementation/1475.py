8ef80219a37f440ba29dbc3fed9209af93f763ed
WooYong Kim <wykim970@gmail.com>
2021-05-14 23:49:56 +0900

    modify content

--- Baekjoon/Implementation/1475.py
+++ Baekjoon/Implementation/1475.py
@@ -2,14 +2,12 @@
 
 n = input()
 n_list = [int(i) for i in n]
-count = [0,0,0,0,0,0,0,0,0]     # 0 ~ 8의 개수 count
-                                # 6, 9는 같은 숫자 취급
+count = [0,0,0,0,0,0,0,0,0]     
 
 for j in n_list:
     idx = j
     if idx == 9:
         idx = 6
     count[idx] += 1
-count[6] = (count[6]+1)//2      # 666 -> 1.5 -> 2세트
-                                # 66666 -> 2.5 -> 3세트
+count[6] = (count[6]+1)//2      
 print(max(count))

1146800b8cecde4c99b6cd7cf90f5bc79f2974d8
WooYong Kim <wykim970@gmail.com>
2021-05-19 20:32:55 +0900

    21/05/19 파일명 정렬 solved

--- Programmers/Level 2/FileNameSort.py
+++ Programmers/Level 2/FileNameSort.py
@@ -2,5 +2,29 @@
 # Input
 files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
 
+def makeHeadNumber(file):
+    h = 0
+
+    for s in file:
+        if s.isdigit():
+            break
+        h += 1
+
+    head = file[:h].lower()
+
+    number = file[h:h+5]
+    n = 0
+
+    for s in number:
+        if not s.isdigit():
+            break
+        n += 1
+
+    return [head, int(number[:n])]
+
 def solution(files):
-    
+    files.sort(key = lambda x : makeHeadNumber(x))
+
+    return files
+
+print(solution(files))

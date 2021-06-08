95f51fa660a01ac042ecba141bd3632250275dcf
WooYong Kim <wykim970@gmail.com>
2021-06-08 16:28:24 +0900

    21/06/08 다트 게임 solved

--- Programmers/Level 1/Kakao/DartGame.py
+++ Programmers/Level 1/Kakao/DartGame.py
@@ -1,9 +1,37 @@
 # 다트 게임 2018 KAKAO BLIND RECRUITMENT
 # Input
-result = '1D2S#10S'
+result = '1D#2S*3S'
 
-def solution(dartResult):
-    
+def solution(result):
+    scoreList = []
+    score = 0
+    flag = False
+    result = result.replace('10','a')
 
+    for r in result:
+        if r in 'SDT':
+            if r == 'D':
+                score *= score
+            elif r == 'T':
+                score = score * score * score
+        elif r in '*#':
+            if r == '*':
+                score *= 2
+                if scoreList:
+                    scoreList[-1] *= 2
+            else:
+                score = -(score)
+        else:
+            if flag:
+                scoreList.append(score)
+            if r == 'a':
+                score = 10
+            else:
+                score = int(r)
+            flag = True
+
+    scoreList.append(score)
+    answer = sum(scoreList)
     return answer
+
 print(solution(result))

fc4fa5b6a2a7be01171c588de13511ff2af6ee40
WooYong Kim <wykim970@gmail.com>
2021-06-08 17:16:19 +0900

    21/06/08 다리를 지나는 트럭 solved again

--- Programmers/Level 2/CrossTheBridge.py
+++ Programmers/Level 2/CrossTheBridge.py
@@ -7,42 +7,23 @@ truck = [7,4,5,6]
 from collections import deque
 
 def solution(length, weight, truck):
-    crossed = []
-    crossing = []
+    truck.reverse()
+    queue_t = deque()
+    queue_c = deque()
     time = 0
-    weight_t = 0
-    deq_t = deque(truck)
-    deq_c = deque(crossing)
 
-    deq_c.append([deq_t.popleft(),0])
-    weight_t += deq_c[0][0]
-    deq_c[0][1] += 1
-    time += 1
-
-    while deq_c:
-        if deq_t and weight_t + deq_t[0] <= weight:
-            deq_c.append([deq_t.popleft(),0])
-            weight_t += deq_c[0][0]
-            
-            for t in deq_c:
-                t[1] += 1
-            time += 1
-        else:
-            for t in deq_c:
-                t[1] += 1
-            time += 1
-    
-        if deq_c and deq_c[0][1] > length:
-            weight_t -= deq_c[0][0]
-            crossed.append(deq_c.popleft())
-
-            if deq_t and weight_t + deq_t[0] <= weight:
-                deq_c.append([deq_t.popleft(),0])
-                weight_t += deq_c[0][0]
-
-                for t in deq_c:
-                    t[1] += 1
-                time += 1
+    while truck or queue_c:
+        if queue_t:
+            if queue_t[0] == length:
+                queue_c.popleft()
+                queue_t.popleft()
+        if truck:
+            if sum(queue_c) + truck[-1] <= weight:
+                queue_c.append(truck.pop())
+                queue_t.append(0)
+        for i in range(len(queue_t)):
+            queue_t[i] += 1
+        time += 1
 
     return time
 

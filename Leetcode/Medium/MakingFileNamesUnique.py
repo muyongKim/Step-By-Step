8597b7ca03406f59a80893eae9207500b71641ea
WooYong Kim <wykim970@gmail.com>
2022-02-04 22:52:41 +0900

    22/02/04 LeetCode Making File Names Unique solved

--- Leetcode/Medium/MakingFileNamesUnique.py
+++ Leetcode/Medium/MakingFileNamesUnique.py
@@ -1,22 +1,12 @@
 # 22/02/03
+# 22/02/04 solved
 # Array, Hash Table, String
 # Input
-names = ["kaido","kaido(1)","kaido","kaido(1)"]
+names = ["a", "a(3)", "a", "a", "a"]
 
-def solution(names):
-    files = {}
-
-    for i in range(len(names)):
-        if names[i] in files:
-            start = names[i].rfind("(")
-            end = names[i].rfind(")")   
-            num = names[i][start+1:end]
-            new_name = names[i] + "(" + str(num) + ")"
-            files[new_name] = files[names[i]] + 1
-        else:
-            files[names[i]] = 0
-
-
+# def solution(names):
+    # files = []
+    # 
     # for i in range(len(names)):
     #     if names[i] in files:
     #         for j in range(1, i+1):
@@ -26,7 +16,28 @@ def solution(names):
     #                 break
     #     else:
     #         files.append(names[i])
+    #
+    # return files
+    # 시간 제한 초과
+
+def solution(names):
+    files = {}
+    output = []
+
+    for name in names:
+        if name not in files:
+            output.append(name)
+            files[name] = 1
+        else:
+            k = files[name]
+            
+            while name + f"({k})" in files:
+                k += 1
+            new_name = name + f"({k})"
+            output.append(new_name)
+            files[name] += 1
+            files[new_name] = 1
     
-    return files
+    return output
 
 print(solution(names))
